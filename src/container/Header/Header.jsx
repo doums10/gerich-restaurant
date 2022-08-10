972513be3ca6011115e8577a5f3b772ab68889b9
Mamadou Sylla <m27sylla@gmail.com>
2022-08-10 10:11:24 -0400

    header component started css to do

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { images } from "../../constants";
 import './Header.css';
+import SubHeading from '../../components/SubHeading/SubHeading';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app __header app__wrapper section__padding' id="home">
+  <div className='app__wrapper_info'>
+    <SubHeading title="Chase The New Flavour"/>
+    <h1 className='app__header-h1'>The Key to Find Dining</h1>
+  </div>
+  <div className='app__wrapper_img'></div>
+    
   </div>
 );
 
