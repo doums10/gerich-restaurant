5cacc2ab1c1c90a01affb32b6a780603549b3b83
Mamadou Sylla <m27sylla@gmail.com>
2022-08-17 07:35:45 -0400

    footer responsive done, project done

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,13 +8,16 @@ const Footer = () => (
 	<div className="app__footer section__padding">
 		<FooterOverlay />
 		<Newsletter />
+    
 		<div className="app__footer-links">
+
 			<div className="app__footer-links_contact">
 				<h1 className="app__footer-headtext">Contact Us</h1>
 				<p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
 				<p className="p__opensans">+1 212-344-1230</p>
 				<p className="p__opensans">+1 212-555-1230</p>
 			</div>
+
 			<div className="app__footer-links_logo">
 				<img src={images.gericht} alt="footer_logo" />
 				<p className="p__opensans">
@@ -27,12 +30,15 @@ const Footer = () => (
 					className="spoon__img"
 					style={{ marginTop: "15" }}
 				/>
+
 				<div className="app__footer-links_icons ">
 					<FiFacebook />
 					<FiTwitter />
 					<FiInstagram />
 				</div>
+
 			</div>
+
 			<div className="app__footer-links_work">
 				<h1 className="app__footer-headtext">Working Hours</h1>
 				<p className="p__opensans">Monday-Friday:</p>
@@ -40,10 +46,13 @@ const Footer = () => (
 				<p className="p__opensans">Saturday-Sunday:</p>
 				<p className="p__opensans">07:00am -11:00 pm</p>
 			</div>
+
 		</div>
+
 		<div className="footer__copyright">
 			<p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
 		</div>
+
 	</div>
 );
 
